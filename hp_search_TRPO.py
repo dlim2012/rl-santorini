d4d333b5097958490c21b2ac85855e079fca875d
Jung Hoon Lim <57515449+dlim2012@users.noreply.github.com>
2022-12-07 06:40:24 -0500

    Update hp_search_TRPO.py

--- hp_search_TRPO.py
+++ hp_search_TRPO.py
@@ -4,34 +4,44 @@ import os
 from stable_baselines3.common.env_checker import check_env
 from stable_baselines3.common.monitor import Monitor
 from argparse import ArgumentParser
-import time
-from copy import deepcopy
-import numpy as np
-import matplotlib.pyplot as plt
+from tensorboardX import SummaryWriter
 
-from board_games.Santorini.agents import RandomAgent, RLAgent
+from board_games.Santorini.agents import RandomAgent, RLAgent, MiniMaxAgent
 from board_games.Santorini.board import GameBoard
 from board_games.CustomEnv import CustomEnv
 from utils.evaluate import evaluate_policy
-from utils.tools import time_hr_min_sec, Namespace
 from concurrent.futures import ProcessPoolExecutor
+from multiprocessing import cpu_count
 
 
 def learn(lr, invalid_action_reward, n_steps, cg_damping, n_critic_updates, gae_lambda):
-    start = time.time()
+    total_timesteps = int(2e7) ############ 2e7
+
+    n_iter = total_timesteps // n_steps
+    eval_period = int(1e5) // n_steps ############### int(1e5)
+    if eval_period == 0:
+        eval_period = 1
 
     model_save_dir = 'hp_search/checkpoints/PPO'
     fig_save_dir = 'hp_search/results/PPO'
+    log_dir = 'logs/PPO'
     os.makedirs(model_save_dir, exist_ok=True)
     os.makedirs(fig_save_dir, exist_ok=True)
-
+    os.makedirs(log_dir, exist_ok=True)
     save_name = 'lr%.2e_ir%.2e_ns%.2e_cd%.2e_cu%.2e_gl%.2e' % \
                 (lr, invalid_action_reward, n_steps, cg_damping, n_critic_updates, gae_lambda)
     print(save_name)
 
+    writer = SummaryWriter(os.path.join(log_dir, save_name))
+
     agent = RLAgent(learning=True)
     opponent = RLAgent(learning=False)
-    random_agent = RandomAgent()
+    opponents = [
+        ('random', RandomAgent(), 100), ############################ 100
+        ('minimax1', MiniMaxAgent(maxDepth=1), 100), ############################ 100
+        ('minimax2', MiniMaxAgent(maxDepth=2), 20), ############################ 20
+        ('minimax3', MiniMaxAgent(maxDepth=3), 4), ############################ 4
+    ]
 
     game_board = GameBoard(
         agents=[agent, opponent],
@@ -65,43 +75,40 @@ def learn(lr, invalid_action_reward, n_steps, cg_damping, n_critic_updates, gae_
 
     check_env(env)
 
-    results = []
-    for i in range(100):  ################################################ 1e2
+    for i in range(n_iter):
         print([i],end='')
 
-        # print(env.game_board.agents, env.game_board.agents[0].model == env.game_board.agents[1].model)
-        model.learn(total_timesteps=int(1e5))  ################################################ 1e5
+        model.learn(total_timesteps=n_steps)
 
-        average_rewards, counts = evaluate_policy(
-            model,
-            env,
-            n_eval_episodes=100,  ################################################ 1e3
-            print_result=False,
-            agents=[agent, random_agent],
-            count_invalid_actions=0
-        )
+        if (i+1) % eval_period == 0:
+            for name, opponent, n_eval_episodes in opponents:
+                average_rewards, counts = evaluate_policy(
+                    model,
+                    env,
+                    n_eval_episodes=n_eval_episodes,
+                    print_result=False,
+                    agents=[agent, opponent],
+                    count_invalid_actions=int(1e3)
+                )
 
-        results.append(average_rewards)
+                writer.add_scalar(name, average_rewards, n_steps * (i+1))
+                writer.add_scalar('invalid_action_count_%s' % name, counts['invalid_action_count'], n_steps * (i+1))
 
-    average_rewards, counts = evaluate_policy(
-        model,
-        env,
-        n_eval_episodes=10000,  ##############################################################1e4
-        print_result=False,
-        agents=[agent, random_agent],
-        count_invalid_actions=0
-    )
-
-    print('%d hr %d min %d sec' % time_hr_min_sec(time.time() - start))
-
-    save_name = '%.2f_' % average_rewards + save_name
 
     model.save(os.path.join(model_save_dir, save_name))
 
-    plt.plot(np.arange(len(results)), np.array(results))
-    plt.savefig(os.path.join(fig_save_dir, save_name + '.png'))
-    plt.clf()
-    plt.close()
+    for name, opponent, n_eval_episodes in opponents:
+        average_rewards, counts = evaluate_policy(
+            model,
+            env,
+            n_eval_episodes=n_eval_episodes * 100, ######################
+            print_result=False,
+            agents=[agent, opponent],
+            count_invalid_actions=int(1e3)
+        )
+        writer.add_scalar(name + '_final', average_rewards, 1)
+
+    writer.close()
 
 
 def main():
@@ -113,14 +120,15 @@ def main():
     lr, invalid_action_reward, n_steps, cg_damping, n_critic_updates, gae_lambda = \
         0.03, -10, 10000, 0.1, 10, 0.95
 
-    pool = ProcessPoolExecutor(max_workers=20)
-    #pool.submit(learn, lr, invalid_action_reward, n_steps, cg_damping, n_critic_updates, gae_lambda)
+    pool = ProcessPoolExecutor(max_workers=cpu_count())
+    #learn(lr, invalid_action_reward, n_steps, cg_damping, n_critic_updates, gae_lambda)
+
 
     for lr_ in [0.3, 0.1, 0.03, 0.01, 0.003, 0.001, 0.0003, 0.0001]:
         pool.submit(learn, lr_, invalid_action_reward, n_steps, cg_damping, n_critic_updates, gae_lambda)
 
     return
-
+    """
     for invalid_action_reward_ in [-100, -20, -5, -1]:
         pool.submit(learn, lr, invalid_action_reward_, n_steps, cg_damping, n_critic_updates, gae_lambda)
 
@@ -135,8 +143,7 @@ def main():
 
     for gae_lambda_ in [0.95, 0.98, 1.0]:
         pool.submit(learn, lr, invalid_action_reward, n_steps, cg_damping, n_critic_updates, gae_lambda_)
-
-
+    """
 
 if __name__ == '__main__':
     main()
